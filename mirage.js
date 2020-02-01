78ebbd229d43a92cb92f5840ef8de0e000a8baa8
Jyle-Crennan <60074222+Jyle-Crennan@users.noreply.github.com>
2020-01-31 22:03:20 -0500

    Update mirage.js

--- mirage.js
+++ mirage.js
@@ -44,11 +44,11 @@ bot.on('message', msg => {
     .addField('Who We Are', 'Eyes up Guardians, welcome to The Black Mirage! We strive to create a lasting hardcore Destiny clan with no skill-gap, whose members can be relied on to join any activity and complete it with ease (or at least hold their own at all cost as others fail). Our knowledge and skill will be superior to all we encounter. That is the mission, and I’m VERY happy to have you on board. Thank you for your support and your belief in this idea, keep killing it!')
     .addField('Requirements', '- Be active at a minimum once every 2 weeks\n- Achieve at least **Diamond V** in **Speed** on raid.report\n- Meet all of the speed requirements (*see below*)\n- Achieve at least **Platinum III** in **Full Clears** on raid.report\n- Have **10+ full clears** of the following raids: **LW, SotP, CoS, GoS**\n- Have a minimum of **one Flawless raid** completion of your choice')
     .addField('*Speed Requirements*', '*Leviathan <= 40 minutes*\n*Eater of Worlds <= 35 minutes*\n*Spire of Stars <= 45 minutes*')
-    .addField('__Clan Admins__', '**angrywalrus7**\nBdunn02\nTheBsBSound\nRTTC virulent')
-    .addField('__Server Staff__', '**angrywalrus7**\nAsashin\nJy1e', true)
+    .addField('__Clan Admins__', '**angrywalrus7 (owner)**\nAsashin\nBdunn02\nRTTC virulent\nTheBsBSound', true)
+    .addField('__Server Staff__', '**angrywalrus7 (owner)**\nAsashin\nJy1e', true)
     .setColor(0x58ffe2)
     .setThumbnail(bot.user.avatarURL)
-    .setFooter('Always remember to keep killing it out there, Guardian!')
+    .setFooter("[Note: Any questions about Mirage Ghost should go to @Jy1e]")
     if (msg.content === '?about') {
       msg.delete();
       msg.channel.send(about);
